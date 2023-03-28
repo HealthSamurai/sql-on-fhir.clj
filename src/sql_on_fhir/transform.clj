6c281e3e7c3a164538619a050d3d25ac714fcef5
niquola <niquola@gmail.com>
2023-03-28 08:24:26 +0100

    add support for ndjson

--- src/sql_on_fhir/transform.clj
+++ src/sql_on_fhir/transform.clj
@@ -1,15 +1,20 @@
 (ns sql-on-fhir.transform
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [cheshire.core])
+  (:import (java.io BufferedInputStream BufferedReader BufferedOutputStream BufferedWriter FileInputStream FileOutputStream InputStreamReader OutputStreamWriter)
+           (java.net URL)
+           (java.nio.charset StandardCharsets)
+           (java.util.zip GZIPInputStream GZIPOutputStream)))
 
 (declare transform)
 (defn process-extensions [ctx exts]
   (->> exts
        (map-indexed (fn [i e] [i e]))
-       (reduce (fn [acc [i {url "url" nested "extension" :as ext}]]
+       (reduce (fn [acc [i {url :url nested :extension :as ext}]]
                  (if url
                    (let [k (str/replace (last (str/split url #"/")) #"[^_a-zA-Z0-9]+" "_")]
-                     (update acc k (fn [x]
-                                     (conj (or x []) (assoc (transform ctx ext) "sof_index" i)))))
+                     (update acc k (fn [x] (conj (or x []) (assoc (transform ctx ext) :sof_index i)))))
                    acc))
                {})))
 
@@ -24,27 +29,27 @@
     (->> obj
          (reduce (fn [acc [k v]]
                    (cond
-                     (and (= k "reference") (string? v))
+                     (and (= k :reference) (string? v))
                      (let [[type id] (parse-ref v)]
-                       (assoc acc k v "sof_id" id "type" type))
+                       (assoc acc k v :sof_id id :type type))
 
-                     (and (= k "coding") (sequential? v))
-                     (assoc acc k (transform ctx v) "sof_codes" (->> v (mapv (fn [{s "system" c "code"}] (str s "|" c)))))
+                     (and (= k :coding) (sequential? v))
+                     (assoc acc k (transform ctx v) :sof_codes (->> v (mapv (fn [{s :system c :code}] (str s "|" c)))))
 
                      :else
                      (cond->
                          (if (or (map? v) (vector? v))
                            (let [v' (transform ctx v)]
-                             (if (and (sequential? v') (seq (mapcat "sof_codes" v')))
-                               (let [codes (->> (mapcat #(get % "sof_codes") v')
+                             (if (and (sequential? v') (seq (mapcat :sof_codes v')))
+                               (let [codes (->> (mapcat #(get % :sof_codes) v')
                                                 (into #{})
                                                 (into []))]
                                  (assoc acc (keyword (str "sof_" (name k) "_codes")) codes k v'))
                                (assoc acc k v')))
                            (assoc acc k v))
-                       (= k "extension")
-                       (-> (dissoc "extension")
-                           (assoc "sof_extension" (process-extensions ctx v))))))
+                       (= k :extension)
+                       (-> (dissoc :extension)
+                           (assoc :sof_extension (process-extensions ctx v))))))
                  {}))
 
     (sequential? obj)
@@ -54,10 +59,55 @@
                  []))
     :else obj))
 
-(defn translate-ndjson [input-file output-file]
 
-  )
+(defn gzip-reader [file]
+  (let [stream (FileInputStream. file)
+        bs (BufferedInputStream. stream)
+        gz (GZIPInputStream. bs)
+        br (BufferedReader. (InputStreamReader. gz StandardCharsets/UTF_8))]
+    br))
+
+(defn gzip-writer [file]
+  (let [stream (FileOutputStream. file)
+        bs (BufferedOutputStream. stream)
+        gz (GZIPOutputStream. bs)]
+    (BufferedWriter. (OutputStreamWriter. gz StandardCharsets/UTF_8))))
+
+(defn translate-ndjson [input-file output-file]
+  (with-open [w (if (str/ends-with? input-file ".gz")
+                  (gzip-writer output-file)
+                  (io/writer output-file))]
+    (with-open [rdr (if (str/ends-with? output-file ".gz")
+                      (gzip-reader input-file)
+                      (clojure.java.io/reader (io/file input-file)))]
+      (doseq [line (line-seq rdr)]
+        (.write w (cheshire.core/generate-string (transform {} (cheshire.core/parse-string line keyword))))
+        (.write w "\n")))))
+
+(defn read-ndjson [file]
+  (with-open [rdr (if (str/ends-with? file ".gz")
+                    (gzip-reader file)
+                    (clojure.java.io/reader (io/file file)))]
+    (->> (line-seq rdr)
+         (mapv (fn [x] (cheshire.core/parse-string x keyword))))))
 
 (defn translate-ndjson-directory [input-dir output-dir]
+  (let [output-dir (io/file output-dir)]
+    (when (.exists output-dir)
+      (throw (Exception. (str output-dir " already exists"))))
+    (.mkdir output-dir)
+    (->> (.listFiles (io/file input-dir))
+         (mapv (fn [file]
+                 (when (str/ends-with? (.getName file) ".ndjson")
+                   (let [output-file (io/file output-dir (.getName file))]
+                     (println :create output-file)
+                     (time (translate-ndjson file output-file)))))))))
+
+(comment
+  (translate-ndjson "fhir/Patient.ndjson.gz" "/tmp/pt.ndjson.gz")
+
+  (read-ndjson "/tmp/pt.ndjson.gz")
+
+  (translate-ndjson-directory "fhir" "fhir.sof")
 
   )
--- test/sql_on_fhir/transform_test.clj
+++ test/sql_on_fhir/transform_test.clj
@@ -8,7 +8,7 @@
 
 (t/deftest test-transform
 
-  (def cases (clj-yaml.core/parse-string (slurp "test_cases/transform.yaml") {:keywords false}))
+  (def cases (clj-yaml.core/parse-string (slurp "test_cases/transform.yaml")))
 
   (doseq [[case-name {input "input" output "output"}] cases]
     (println case-name)
@@ -18,6 +18,147 @@
     (matcho/match (sut/transform {} input) output)
 
     )
+
+  (sut/translate-ndjson "test/sql_on_fhir/pt.ndjson" "/tmp/result.ndjson")
+  ;; (sut/translate-ndjson "test/sql_on_fhir/pt.ndjson" "test/sql_on_fhir/pt.sof.ndjson")
+
+  (t/is (= (slurp "test/sql_on_fhir/pt.sof.ndjson") (slurp "/tmp/result.ndjson")))
+
+  (matcho/match
+   (first (sut/read-ndjson "/tmp/result.ndjson"))
+   {:multipleBirthBoolean false,
+    :address
+    [{:sof_extension
+      {:geolocation
+       [{:url "http://hl7.org/fhir/StructureDefinition/geolocation",
+         :sof_extension
+         {:latitude
+          [{:url "latitude", :valueDecimal 42.419087882595335, :sof_index 0}],
+          :longitude
+          [{:url "longitude", :valueDecimal -71.149086039334, :sof_index 1}]},
+         :sof_index 0}]},
+      :line ["785 Gerlach Boulevard Suite 42"],
+      :city "Arlington",
+      :state "MA",
+      :postalCode "02476",
+      :country "US"}],
+    :meta
+    {:profile
+     ["http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient"]},
+    :name
+    [{:use "official",
+      :family "Muller251",
+      :given ["Rueben647" "Antwan357"],
+      :prefix ["Mr."]}],
+    :birthDate "1989-12-02",
+    :resourceType "Patient",
+    :sof_extension
+    {:us_core_race
+     [{:url "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race",
+       :sof_extension
+       {:ombCategory
+        [{:url "ombCategory",
+          :valueCoding
+          {:system "urn:oid:2.16.840.1.113883.6.238",
+           :code "2106-3",
+           :display "White"},
+          :sof_index 0}],
+        :text [{:url "text", :valueString "White", :sof_index 1}]},
+       :sof_index 0}],
+     :us_core_ethnicity
+     [{:url "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity",
+       :sof_extension
+       {:ombCategory
+        [{:url "ombCategory",
+          :valueCoding
+          {:system "urn:oid:2.16.840.1.113883.6.238",
+           :code "2186-5",
+           :display "Not Hispanic or Latino"},
+          :sof_index 0}],
+        :text
+        [{:url "text", :valueString "Not Hispanic or Latino", :sof_index 1}]},
+       :sof_index 1}],
+     :patient_mothersMaidenName
+     [{:url "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName",
+       :valueString "Georgann131 Fritsch593",
+       :sof_index 2}],
+     :us_core_birthsex
+     [{:url "http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex",
+       :valueCode "M",
+       :sof_index 3}],
+     :patient_birthPlace
+     [{:url "http://hl7.org/fhir/StructureDefinition/patient-birthPlace",
+       :valueAddress {:city "Marshfield", :state "Massachusetts", :country "US"},
+       :sof_index 4}],
+     :disability_adjusted_life_years
+     [{:url
+       "http://synthetichealth.github.io/synthea/disability-adjusted-life-years",
+       :valueDecimal 0.0,
+       :sof_index 5}],
+     :quality_adjusted_life_years
+     [{:url
+       "http://synthetichealth.github.io/synthea/quality-adjusted-life-years",
+       :valueDecimal 33.0,
+       :sof_index 6}]},
+    :communication
+    [{:language
+      {:coding [{:system "urn:ietf:bcp:47", :code "en-US", :display "English"}],
+       :sof_codes ["urn:ietf:bcp:47|en-US"],
+       :text "English"}}],
+    :id "b0e2bc9c-11c2-3ccf-a4f9-a93df80344b8",
+    :identifier
+    [{:system "https://github.com/synthetichealth/synthea",
+      :value "b0e2bc9c-11c2-3ccf-a4f9-a93df80344b8"}
+     {:type
+      {:coding
+       [{:system "http://terminology.hl7.org/CodeSystem/v2-0203",
+         :code "MR",
+         :display "Medical Record Number"}],
+       :sof_codes ["http://terminology.hl7.org/CodeSystem/v2-0203|MR"],
+       :text "Medical Record Number"},
+      :system "http://hospital.smarthealthit.org",
+      :value "b0e2bc9c-11c2-3ccf-a4f9-a93df80344b8"}
+     {:type
+      {:coding
+       [{:system "http://terminology.hl7.org/CodeSystem/v2-0203",
+         :code "SS",
+         :display "Social Security Number"}],
+       :sof_codes ["http://terminology.hl7.org/CodeSystem/v2-0203|SS"],
+       :text "Social Security Number"},
+      :system "http://hl7.org/fhir/sid/us-ssn",
+      :value "999-30-4461"}
+     {:type
+      {:coding
+       [{:system "http://terminology.hl7.org/CodeSystem/v2-0203",
+         :code "DL",
+         :display "Driver's License"}],
+       :sof_codes ["http://terminology.hl7.org/CodeSystem/v2-0203|DL"],
+       :text "Driver's License"},
+      :system "urn:oid:2.16.840.1.113883.4.3.25",
+      :value "S99998964"}
+     {:type
+      {:coding
+       [{:system "http://terminology.hl7.org/CodeSystem/v2-0203",
+         :code "PPN",
+         :display "Passport Number"}],
+       :sof_codes ["http://terminology.hl7.org/CodeSystem/v2-0203|PPN"],
+       :text "Passport Number"},
+      :system
+      "http://standardhealthrecord.org/fhir/StructureDefinition/passportNumber",
+      :value "X77003476X"}],
+    :telecom [{:system "phone", :value "555-743-6201", :use "home"}],
+    :gender "male",
+    :maritalStatus
+    {:coding
+     [{:system "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus",
+       :code "M",
+       :display "M"}],
+     :sof_codes ["http://terminology.hl7.org/CodeSystem/v3-MaritalStatus|M"],
+     :text "M"},
+    :text
+    {:status "generated",
+     :div
+     "<div xmlns=\"http://www.w3.org/1999/xhtml\">Generated by <a href=\"https://github.com/synthetichealth/synthea\">Synthea</a>.Version identifier: master-branch-latest\n .   Person seed: -1563869585931119458  Population seed: 1679004005360</div>"}})
 
   )
 
